f02e007969d2f5e298eb4e323142f4da0b662afe
BritishTeapot <f.kovalyoff@gmail.com>
2024-02-26 23:09:24 +0100

    Added headers for classes

---
+++ src/Path.h
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <vector>
+
+class Route {
+private:
+  std::vector<int> path;
+
+public:
+  Route(std::vector<int> path);
+  int getPosition(int index);
+  static Route fromFile(std::ifstream file);
+};
---
+++ src/System.h
@@ -0,0 +1,18 @@
+#include "Path.h"
+#include <fstream>
+#include <map>
+#include <set>
+#include <utility>
+class TrackGraph {
+private:
+  std::map<int, std::set<int>> adjacencyMap;
+  std::map<int, std::set<int>> conflictMap;
+
+public:
+  static TrackGraph fromFile(std::ifstream &file);
+  bool isRouteValid(Route &route);
+  std::tuple<std::vector<Route>::iterator, std::vector<Route>::iterator,
+             int // position of the conflicting verticies
+             >
+  findConflicting(std::vector<Route> routes);
+};
