a466c85b8eb17d050fa7f9d889f4e976c2c07d03
BritishTeapot <f.kovalyoff@gmail.com>
2024-03-01 22:47:09 +0100

    Made fromFile to reject a -> a subpaths

--- src/route.cpp
+++ src/route.cpp
@@ -1,4 +1,7 @@
 #include "route.h"
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
 #include <vector>
 
 Route::Route(std::vector<int> path) { this->path = path; }
@@ -16,6 +19,18 @@ Route Route::fromFile(std::ifstream &file) {
   for (int i = 0; i < lenght; i++) {
     int element;
     file >> element;
+    /*
+     * Following code checks for subpaths like "a -> a" (a.k.a. duplicate
+     * consecutive veritcies), which do not make sense in the job-shop model.
+     * Techiaclly speaking they do not pose any significant problem,
+     * however if we assume no consecutive duplicates are present,
+     * code can be simplified a lot.
+     */
+    if ((i != lenght - 1) && (element == path.back())) {
+      std::cout << "Bad file format: consecutive duplicate path veritcies."
+                << std::endl;
+      exit(EXIT_FAILURE);
+    }
     path.push_back(element);
   }
 
