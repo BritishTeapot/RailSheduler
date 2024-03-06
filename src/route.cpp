6a41d0a7c818c9b0b54f3c22ef66d285339ba40a
BritishTeapot <f.kovalyoff@gmail.com>
2024-03-06 11:23:15 +0100

    Made a struct for route vertex

--- src/route.cpp
+++ src/route.cpp
@@ -1,27 +1,40 @@
 #include "route.h"
+#include <cstddef>
 #include <cstdint>
 #include <cstdlib>
 #include <iostream>
 #include <ostream>
+#include <utility>
 #include <vector>
 
-Route::Route(std::vector<int64_t> path) { this->path = path; }
+Route::Route(std::vector<std::pair<int64_t, int64_t>> path) {
+  std::vector<routeVertex> new_path;
+  for (auto i : path) {
+    routeVertex rv;
+    rv.track = i.first;
+    rv.min_time = i.second;
+    new_path.push_back(rv);
+  }
+  this->path = new_path;
+}
 
 Route::Route() {} // default constructor for an empty path
 
-int64_t Route::getPosition(int index) { return path.at(index); }
+int64_t Route::getPosition(int index) { return path.at(index).track; }
 
 int Route::getLength() { return path.size(); }
 
 Route Route::fromFile(std::ifstream &file) {
-  int lenght;
-  std::vector<int64_t> path;
+  size_t lenght;
+  std::vector<routeVertex> path;
 
   file >> lenght;
 
-  for (int i = 0; i < lenght; i++) {
-    int element;
-    file >> element;
+  for (size_t i = 0; i < lenght; i++) {
+    int64_t track;
+    int64_t time;
+    file >> track;
+    file >> track;
     /*
      * Following code checks for subpaths like "a -> a" (a.k.a. duplicate
      * consecutive veritcies), which do not make sense in the job-shop model.
@@ -29,12 +42,15 @@ Route Route::fromFile(std::ifstream &file) {
      * however if we assume no consecutive duplicates are present,
      * code can be simplified a lot.
      */
-    if ((i != 0) && (element == path.back())) {
+    if ((i != 0) && (track == path.back().track)) {
       std::cout << "Bad file format: consecutive duplicate path veritcies."
                 << std::endl;
       exit(EXIT_FAILURE);
     }
-    path.push_back(element);
+    routeVertex rv;
+    rv.track = track;
+    rv.min_time = time;
+    path.push_back(rv);
   }
 
   return Route(path);
--- src/route.h
+++ src/route.h
@@ -1,18 +1,31 @@
-
 #ifndef ROUTE_H
 #define ROUTE_H
 
 #include <cstdint>
+#include <cstring>
 #include <fstream>
+#include <utility>
 #include <vector>
+
+struct routeVertex {
+  int64_t track;
+
+  // here min_time is minimum time the
+  // train needs to spend on this track to
+  // leave it in seconds
+  int64_t min_time;
+  // the time has to be an integer because CP-SAT
+  // only works with integers
+};
 
 class Route {
 private:
-  std::vector<int64_t> path;
+  std::vector<routeVertex> path;
 
 public:
   Route();
-  Route(std::vector<int64_t> path);
+  Route(std::vector<routeVertex> path);
+  Route(std::vector<std::pair<int64_t, int64_t>> path);
   int64_t getPosition(int index);
   int getLength();
   static Route fromFile(std::ifstream &file);
